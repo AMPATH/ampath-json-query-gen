ac20d3cf16a4cf265d72041674b3d3bd422f5b62
Nicky Kibor <nickykibor@Nickys-MacBook-Pro.local>
2018-03-12 17:29:50 +0300

    Added functions to add columns to the patient list dynamic query

--- src/base-patientlist-gen.js
+++ src/base-patientlist-gen.js
@@ -14,4 +14,33 @@ export default class BasePatientListGen {
     return {};
   }
 
+  addMissingColumns(destinationSchema, sourceSchema) {
+    sourceSchema.columns.forEach(column => {
+      if (!this.columnExistsInColumnArray(column, destinationSchema.columns)) {
+        destinationSchema.columns.push(column);
+      }
+    });
+  }
+
+  columnExistsInColumnArray(column, columnArray) {
+
+    for(var i = 0; i < columnArray.length; i++) {
+      if(this.willColumnTitlesCollide(column, columnArray[i])){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  willColumnTitlesCollide(columnA, columnB){
+    if((columnA.alias || columnB.alias) && columnA.alias === columnB.alias) {
+      return true;
+    }
+
+    if(columnA.column && columnB.column && columnA.column === columnB.column) {
+      return true;
+    }
+    return false;
+  }
+
 }
--- test/base-patientlist-gen.spec.js
+++ test/base-patientlist-gen.spec.js
@@ -12,12 +12,94 @@ const expect = chai.expect;
 const should = chai.should;
 let gen;
 
-describe('BasePatientListGen', () => {
+describe('BasePatientListGen:', () => {
   beforeEach(() => {
-    gen = new BasePatientListGen(null,null,null);
+    gen = new BasePatientListGen(null, null, null);
   });
   it('should be defined', () => {
     expect(gen).to.exist;
     expect(gen.generatePatientListSchema()).not.be.null;
   });
+
+  it('should add columns missing in the base report but exists' +
+    ' in the template report', () => {
+
+      let baseReport = {
+        "columns": [
+          {
+            "type": "column",
+            "alias": "gender",
+            "column": "hmrd.gender"
+          },
+          {
+            "type": "column",
+            "column": "hmrd.person_id"
+          },
+          {
+            "type": "column",
+            "alias": "person_id",
+            "column": "hmrd.person_id"
+          }
+        ]
+      };
+
+      let templateReport = {
+        "columns": [
+          {
+            "type": "simple_column",
+            "alias": "person_id",
+            "column": "t1.person_id"
+          },
+          {
+            "type": "simple_column",
+            "alias": "person_id_2",
+            "column": "t1.person_id"
+          },
+          {
+            "type": "derived_column",
+            "alias": "person_name",
+            "expression_type": "simple_expression",
+            "expression_options": {
+              "expression": " CONCAT(COALESCE(person_name.given_name, ''), ' ', COALESCE(person_name.middle_name, ''), ' ', COALESCE(person_name.family_name, ''))"
+            }
+          }
+        ]
+      };
+
+      let expectedBaseReport = {
+        "columns": [
+          {
+            "type": "column",
+            "alias": "gender",
+            "column": "hmrd.gender"
+          },
+          {
+            "type": "column",
+            "column": "hmrd.person_id"
+          },
+          {
+            "type": "column",
+            "alias": "person_id",
+            "column": "hmrd.person_id"
+          },
+          {
+            "type": "simple_column",
+            "alias": "person_id_2",
+            "column": "t1.person_id"
+          },
+          {
+            "type": "derived_column",
+            "alias": "person_name",
+            "expression_type": "simple_expression",
+            "expression_options": {
+              "expression": " CONCAT(COALESCE(person_name.given_name, ''), ' ', COALESCE(person_name.middle_name, ''), ' ', COALESCE(person_name.family_name, ''))"
+            }
+          }
+        ]
+      };
+
+      gen.addMissingColumns(baseReport, templateReport);
+
+      expect(baseReport).to.deep.equal(expectedBaseReport);
+    });
 });
