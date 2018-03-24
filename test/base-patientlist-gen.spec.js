4b6fec4f208a81e802ecaf3d30ad0e3d31421dec
Nicky Kibor <nickykibor@Nickys-MacBook-Pro.local>
2018-03-24 09:59:15 +0300

    Fixed derived filters

--- test/base-patientlist-gen.spec.js
+++ test/base-patientlist-gen.spec.js
@@ -46,7 +46,7 @@ describe('BasePatientListGen:', () => {
 
   });
 
-  it('should generate the patient list report', ()=>{
+  it('should generate the patient list report', () => {
 
     let baseSchema = {
       columns: [],
@@ -67,7 +67,7 @@ describe('BasePatientListGen:', () => {
     };
 
     let expectedDynamicQuery = {
-      params: {a: 'a'},
+      params: { a: 'a' },
       generated: baseSchema
     };
 
@@ -147,181 +147,181 @@ describe('BasePatientListGen:', () => {
   it('should add columns missing in the base report but exists' +
     ' in the template report', () => {
 
-    let baseReport = {
-      'columns': [
-        {
-          'type': 'column',
-          'alias': 'gender',
-          'column': 'hmrd.gender'
-        },
-        {
-          'type': 'column',
-          'column': 'hmrd.person_id'
-        },
-        {
-          'type': 'column',
-          'alias': 'person_id',
-          'column': 'hmrd.person_id'
-        }
-      ]
-    };
-
-    let templateReport = {
-      'columns': [
-        {
-          'type': 'simple_column',
-          'alias': 'person_id',
-          'column': 't1.person_id'
-        },
-        {
-          'type': 'simple_column',
-          'alias': 'person_id_2',
-          'column': 't1.person_id'
-        },
-        {
-          'type': 'derived_column',
-          'alias': 'person_name',
-          'expressionType': 'simple_expression',
-          'expressionOptions': {
-            'expression': " CONCAT(COALESCE(person_name.given_name, ''), ' ', COALESCE(person_name.middle_name, ''), ' ', COALESCE(person_name.family_name, ''))"
+      let baseReport = {
+        'columns': [
+          {
+            'type': 'column',
+            'alias': 'gender',
+            'column': 'hmrd.gender'
+          },
+          {
+            'type': 'column',
+            'column': 'hmrd.person_id'
+          },
+          {
+            'type': 'column',
+            'alias': 'person_id',
+            'column': 'hmrd.person_id'
           }
-        }
-      ]
-    };
+        ]
+      };
 
-    let expectedBaseReport = {
-      'columns': [
-        {
-          'type': 'column',
-          'alias': 'gender',
-          'column': 'hmrd.gender'
-        },
-        {
-          'type': 'column',
-          'column': 'hmrd.person_id'
-        },
-        {
-          'type': 'column',
-          'alias': 'person_id',
-          'column': 'hmrd.person_id'
-        },
-        {
-          'type': 'simple_column',
-          'alias': 'person_id_2',
-          'column': 't1.person_id'
-        },
-        {
-          'type': 'derived_column',
-          'alias': 'person_name',
-          'expressionType': 'simple_expression',
-          'expressionOptions': {
-            'expression': " CONCAT(COALESCE(person_name.given_name, ''), ' ', COALESCE(person_name.middle_name, ''), ' '," +
+      let templateReport = {
+        'columns': [
+          {
+            'type': 'simple_column',
+            'alias': 'person_id',
+            'column': 't1.person_id'
+          },
+          {
+            'type': 'simple_column',
+            'alias': 'person_id_2',
+            'column': 't1.person_id'
+          },
+          {
+            'type': 'derived_column',
+            'alias': 'person_name',
+            'expressionType': 'simple_expression',
+            'expressionOptions': {
+              'expression': " CONCAT(COALESCE(person_name.given_name, ''), ' ', COALESCE(person_name.middle_name, ''), ' ', COALESCE(person_name.family_name, ''))"
+            }
+          }
+        ]
+      };
+
+      let expectedBaseReport = {
+        'columns': [
+          {
+            'type': 'column',
+            'alias': 'gender',
+            'column': 'hmrd.gender'
+          },
+          {
+            'type': 'column',
+            'column': 'hmrd.person_id'
+          },
+          {
+            'type': 'column',
+            'alias': 'person_id',
+            'column': 'hmrd.person_id'
+          },
+          {
+            'type': 'simple_column',
+            'alias': 'person_id_2',
+            'column': 't1.person_id'
+          },
+          {
+            'type': 'derived_column',
+            'alias': 'person_name',
+            'expressionType': 'simple_expression',
+            'expressionOptions': {
+              'expression': " CONCAT(COALESCE(person_name.given_name, ''), ' ', COALESCE(person_name.middle_name, ''), ' '," +
                 " COALESCE(person_name.family_name, ''))"
+            }
           }
-        }
-      ]
-    };
+        ]
+      };
 
-    gen.addMissingColumns(baseReport, templateReport);
+      gen.addMissingColumns(baseReport, templateReport);
 
-    expect(baseReport).to.deep.equal(expectedBaseReport);
+      expect(baseReport).to.deep.equal(expectedBaseReport);
 
-    // handle other test cases in related functions
-    // willColumnTitlesCollide case similar column members
-    expect(gen.willColumnTitlesCollide({
-      column: 'same'
-    }, {
-      column: 'same'
-    })).to.be.true;
-  });
+      // handle other test cases in related functions
+      // willColumnTitlesCollide case similar column members
+      expect(gen.willColumnTitlesCollide({
+        column: 'same'
+      }, {
+          column: 'same'
+        })).to.be.true;
+    });
 
   it('should create sources member as specified in the dynamic query generation directives' +
     ' in aggregate report schema, in addition to base schema sources', () => {
-    let aggregateSchema = {
-      'dynamicJsonQueryGenerationDirectives': {
-        'patientListGenerator': {
-          'generatingDirectives': {
-            'joinDirectives': {
-              'joinType': 'INNER',
-              'joinCondition': '<<base_column>> = <<template_column>>',
-              'baseColumn': 'person_id',
-              'templateColumn': 'person_id'
+      let aggregateSchema = {
+        'dynamicJsonQueryGenerationDirectives': {
+          'patientListGenerator': {
+            'generatingDirectives': {
+              'joinDirectives': {
+                'joinType': 'INNER',
+                'joinCondition': '<<base_column>> = <<template_column>>',
+                'baseColumn': 'person_id',
+                'templateColumn': 'person_id'
+              }
             }
           }
         }
-      }
-    };
+      };
 
-    let baseSchema = {
-      'sources': [
-        {
-          'table': 'etl.hiv_monthly_report_dataset',
-          'alias': 'hmrd'
-        }
-      ]
-    };
+      let baseSchema = {
+        'sources': [
+          {
+            'table': 'etl.hiv_monthly_report_dataset',
+            'alias': 'hmrd'
+          }
+        ]
+      };
 
-    let templateSchema = {
-      'sources': [
-        {
-          'table': 'amrs.person',
-          'alias': 't1'
-        },
-        {
-          'table': 'amrs.person_name',
-          'alias': 'person_name',
-          'join': {
-            'type': 'INNER',
-            'joinCondition': 't1.person_id = person_name.person_id AND (person_name.voided IS NULL || person_name.voided = 0)'
+      let templateSchema = {
+        'sources': [
+          {
+            'table': 'amrs.person',
+            'alias': 't1'
+          },
+          {
+            'table': 'amrs.person_name',
+            'alias': 'person_name',
+            'join': {
+              'type': 'INNER',
+              'joinCondition': 't1.person_id = person_name.person_id AND (person_name.voided IS NULL || person_name.voided = 0)'
+            }
+          },
+          {
+            'table': 'amrs.patient_identifier',
+            'alias': 'id',
+            'join': {
+              'type': 'LEFT OUTER',
+              'joinCondition': 't1.person_id = person_name.person_id AND (person_name.voided IS NULL || person_name.voided = 0)'
+            }
           }
-        },
-        {
-          'table': 'amrs.patient_identifier',
-          'alias': 'id',
-          'join': {
-            'type': 'LEFT OUTER',
-            'joinCondition': 't1.person_id = person_name.person_id AND (person_name.voided IS NULL || person_name.voided = 0)'
-          }
-        }
-      ]
-    };
+        ]
+      };
 
-    let expectedDynamic = {
-      'sources': [
-        {
-          'table': 'etl.hiv_monthly_report_dataset',
-          'alias': 'hmrd'
-        },
-        {
-          'table': 'amrs.person',
-          'alias': 't1',
-          'join': {
-            'type': 'INNER',
-            'joinCondition': 'hmrd.person_id = t1.person_id'
+      let expectedDynamic = {
+        'sources': [
+          {
+            'table': 'etl.hiv_monthly_report_dataset',
+            'alias': 'hmrd'
+          },
+          {
+            'table': 'amrs.person',
+            'alias': 't1',
+            'join': {
+              'type': 'INNER',
+              'joinCondition': 'hmrd.person_id = t1.person_id'
+            }
+          },
+          {
+            'table': 'amrs.person_name',
+            'alias': 'person_name',
+            'join': {
+              'type': 'INNER',
+              'joinCondition': 't1.person_id = person_name.person_id AND (person_name.voided IS NULL || person_name.voided = 0)'
+            }
+          },
+          {
+            'table': 'amrs.patient_identifier',
+            'alias': 'id',
+            'join': {
+              'type': 'LEFT OUTER',
+              'joinCondition': 't1.person_id = person_name.person_id AND (person_name.voided IS NULL || person_name.voided = 0)'
+            }
           }
-        },
-        {
-          'table': 'amrs.person_name',
-          'alias': 'person_name',
-          'join': {
-            'type': 'INNER',
-            'joinCondition': 't1.person_id = person_name.person_id AND (person_name.voided IS NULL || person_name.voided = 0)'
-          }
-        },
-        {
-          'table': 'amrs.patient_identifier',
-          'alias': 'id',
-          'join': {
-            'type': 'LEFT OUTER',
-            'joinCondition': 't1.person_id = person_name.person_id AND (person_name.voided IS NULL || person_name.voided = 0)'
-          }
-        }
-      ]
-    };
+        ]
+      };
 
-    gen.addSourcesToGeneratedSchema(aggregateSchema, baseSchema, templateSchema);
-    expect(baseSchema).to.deep.equal(expectedDynamic);
-  });
+      gen.addSourcesToGeneratedSchema(aggregateSchema, baseSchema, templateSchema);
+      expect(baseSchema).to.deep.equal(expectedDynamic);
+    });
 
   it('should add grouping functions present in the template', () => {
 
@@ -408,7 +408,7 @@ describe('BasePatientListGen:', () => {
         'conditionJoinOperator': 'AND',
         'conditions': [
           {
-            'filterActingOn': 'simple_column',
+            'filterType': 'tableColumns',
             'conditionExpression': 'endDate = <<@endDate>>'
           }
         ]
@@ -426,22 +426,25 @@ describe('BasePatientListGen:', () => {
       'conditionJoinOperator': 'AND',
       'conditions': [
         {
-          'filterActingOn': 'simple_column',
+          'filterType': 'tableColumns',
           'conditionExpression': 'endDate = <<@endDate>>'
         },
         {
-          'filterActingOn': 'simple_column',
+          'filterType': 'tableColumns',
           'conditionExpression': 'gender = "F"',
+          'parameterName': '',
           'dynamicallyGenerated': true
         },
         {
-          'filterActingOn': 'derived_column',
+          'filterType': 'expressionColumns',
           'conditionExpression': 'hmrd.age between 1 and 9',
+          'parameterName': '',
           'dynamicallyGenerated': true
         },
         {
-          'filterActingOn': 'derived_column',
+          'filterType': 'expressionColumns',
           'conditionExpression': '1 = (CASE WHEN status = "active" AND on_art_this_month = 1 THEN 1 ELSE NULL)',
+          'parameterName': '',
           'dynamicallyGenerated': true
         }
       ]
@@ -457,8 +460,9 @@ describe('BasePatientListGen:', () => {
     // getCaseExpressionColumnFilterObject case else
     expect(gen.getCaseExpressionColumnFilterObject(baseSchema.columns[1], 'older_than_9'))
       .to.deep.equal({
-        filterActingOn: 'derived_column',
+        'filterType': 'expressionColumns',
         conditionExpression: '!((hmrd.age between 0 and 1) AND(hmrd.age between 1 and 9))',
+        'parameterName': '',
         dynamicallyGenerated: true
       });
 
